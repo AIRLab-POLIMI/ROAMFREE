79bb77bb6147a1403cc192ffbe67067bca014362
davide <davide.cucci@epfl.ch>
2015-10-14 08:56:20 +0200

    improved visualization for LinearlyInterpolatedEuclideanParameters, now it shows also covariances if they are available

--- _development/Matlab/PluginViewer/plugins/LinearlyInterpolatedEuclideanParameter.m
+++ _development/Matlab/PluginViewer/plugins/LinearlyInterpolatedEuclideanParameter.m
@@ -3,33 +3,37 @@ function LinearlyInterpolatedEuclideanParameter(area, globalConfig, pluginConfig
 pf = sprintf('%s%s.log', globalConfig.logPath, pluginConfig.parameterName);
 
 if exist(pf, 'file')
-   par = sortByT(stubbornLoad(pf));
-   
-   subplot('Position', squeezeArea(area,0.02))   
-   hold on
-   
-   plot(par(:,1)-par(1,1), par(:, 3:2+pluginConfig.parameterSize), '-o');
-   
-   if strcmp(pluginConfig.parameterName(end-5:end),'_Ba_GM') == 1              
-       ihndlf = [globalConfig.logPath pluginConfig.parameterName(1:end-6) '.log'];
-       
-       ihndl = sortByT(stubbornLoad(ihndlf));
-       
-       plot(ihndl(:,1)-par(1,1), ihndl(:, [13:15]+22), '--');   
-   end
-   
-   if strcmp(pluginConfig.parameterName(end-2:end),'_Bw') == 1
-       
-       ihndlf = [globalConfig.logPath pluginConfig.parameterName(1:end-3) '.log'];
-       
-       ihndl = sortByT(stubbornLoad(ihndlf));
-       
-       plot(ihndl(:, [25:27]+22), '--');   
-   end 
-   
-   title(pluginConfig.parameterName)   
-   
-   hold off
+    par = sortByT(stubbornLoad(pf));
+
+    subplot('Position', squeezeArea(area,0.02))   
+    hold on
+
+    CO = get(gcf,'DefaultAxesColorOrder');
+    
+    % for single vertices duplicate the first row
+    if size(par,1) == 1 
+        par(2,1) = par(1,1)+eps;
+        par(2,2:end)=par(1,2:end);
+    end
+
+    for i = 0:(pluginConfig.parameterSize-1)
+        plot(par(:,1)-par(1,1), par(:, 3+i), '-o', 'MarkerSize',3, 'Color', CO(i+1,:));
+
+        if par(1,3+pluginConfig.parameterSize) ~= Inf
+            pos = i*(pluginConfig.parameterSize+1)-i*(i+1)/2;
+
+            plot(par(:,1)-par(1,1), par(:, 3+i)+sqrt(par(:,3+pluginConfig.parameterSize+pos)), 'LineStyle','--', 'Color', CO(i+1,:));
+            plot(par(:,1)-par(1,1), par(:, 3+i)-sqrt(par(:,3+pluginConfig.parameterSize+pos)), 'LineStyle','--', 'Color', CO(i+1,:));        
+        end   
+    end
+    
+    axis tight
+
+    grid on
+
+    title(strrep(pluginConfig.parameterName,'_','\_'))
+
+    hold off
 end
 
 end
