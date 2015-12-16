c1742a66aaa0df2bff871aa293a1d4391df1b4ef
davide <davide.cucci@epfl.ch>
2015-12-16 11:55:11 +0100

    default plot on monitor 1 for tests IMUHandlerFusionTest switches to StochasticProcess plugin

--- _development/Matlab/PluginViewer/configs/ROAMtest/configIMUGPSFusionTest.m
+++ _development/Matlab/PluginViewer/configs/ROAMtest/configIMUGPSFusionTest.m
@@ -6,8 +6,8 @@ config = struct();
 ss = get(0,'MonitorPositions'); % gets screen coordinates
     
 config.global.layout = 'horizontal';
-%config.global.figureOuterPosition = [ss(1,1) ss(1,2) ss(1,3) ss(1,4)]; % monitor 1
-config.global.figureOuterPosition = [ss(2,1) ss(2,2) ss(2,3) ss(2,4)]; % monitor 2
+config.global.figureOuterPosition = [ss(1,1) ss(1,2) ss(1,3) ss(1,4)]; % monitor 1
+% config.global.figureOuterPosition = [ss(2,1) ss(2,2) ss(2,3) ss(2,4)]; % monitor 2
 config.global.logPath = '/tmp/roamfree/';
 
 config.global.plugins = {'Trajectory', 'GenericEdge', 'GenericEdge', 'GenericEdge', 'LinearlyInterpolatedEuclideanParameter', 'LinearlyInterpolatedEuclideanParameter'};
--- _development/Matlab/PluginViewer/configs/ROAMtest/configIMUHandlerFusionTest.m
+++ _development/Matlab/PluginViewer/configs/ROAMtest/configIMUHandlerFusionTest.m
@@ -6,29 +6,36 @@ config = struct();
 ss = get(0,'MonitorPositions'); % gets screen coordinates
     
 config.global.layout = 'horizontal';
-%config.global.figureOuterPosition = [ss(1,1) ss(1,2) ss(1,3) ss(1,4)]; % monitor 1
+% config.global.figureOuterPosition = [ss(1,1) ss(1,2) ss(1,3) ss(1,4)]; % monitor 1
 config.global.figureOuterPosition = [ss(2,1) ss(2,2) ss(2,3) ss(2,4)]; % monitor 2
 config.global.logPath = '/tmp/roamfree/';
 
-config.global.plugins = {'Trajectory', 'GenericEdge', 'GenericEdge', 'GenericEdge', 'LinearlyInterpolatedEuclideanParameter', 'LinearlyInterpolatedEuclideanParameter'};
+config.global.plugins = {};
+config.pluginConfig = {};
 
-config.pluginConfig{1}.axesLenght = 0.5;
+config.global.plugins{end+1} = 'Trajectory';
+config.pluginConfig{end+1}.axesLenght = 0.5;
 config.pluginConfig{end}.sensorName = 'GPS'; %display also measures from an AbsolutePosition sensor
  
+config.global.plugins{end+1} = 'GenericEdge';
 config.pluginConfig{end+1}.sensorName = 'GPS';
 config.pluginConfig{end}.errorSize = 3;
 config.pluginConfig{end}.measureSize = 3;
 
+config.global.plugins{end+1} = 'GenericEdge';
 config.pluginConfig{end+1}.sensorName = 'IMUintegralDeltaP';
 config.pluginConfig{end}.errorSize = 3;
 config.pluginConfig{end}.measureSize = 27;
 
+config.global.plugins{end+1} = 'GenericEdge';
 config.pluginConfig{end+1}.sensorName = 'IMUintegralDeltaQ';
 config.pluginConfig{end}.errorSize = 4;
 config.pluginConfig{end}.measureSize = 16;
 
-config.pluginConfig{end+1}.parameterName = 'IMUintegralDeltaP_Ba_GM';
+config.global.plugins{end+1} = 'StochasticProcess';
+config.pluginConfig{end+1}.parameterName = 'IMUintegralDeltaP_Ba';
 config.pluginConfig{end}.parameterSize = 3;
 
-config.pluginConfig{end+1}.parameterName = 'IMUintegralDeltaP_Ba_RC';
+config.global.plugins{end+1} = 'StochasticProcess';
+config.pluginConfig{end+1}.parameterName = 'IMUintegralDeltaP_Bw';
 config.pluginConfig{end}.parameterSize = 3;
