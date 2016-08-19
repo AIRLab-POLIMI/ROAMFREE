28fd9c8bfb456d8ec3c5ed7d62c5d3cfd444dfd2
davide <davide.cucci@epfl.ch>
2016-08-19 17:04:44 +0200

    added autoscaling of axes based on std added possibility to plot vertical lines in correspondance of events

--- _development/Matlab/PluginViewer/plugins/GenericEdge.m
+++ _development/Matlab/PluginViewer/plugins/GenericEdge.m
@@ -21,6 +21,7 @@ if exist(edgef, 'file')
         else
             subplot('Position', squeezeArea(area,0.02))
         end
+        hold on
 
         err = edge(:,(23+pluginConfig.measureSize):(23+pluginConfig.measureSize+pluginConfig.errorSize-1));
                 
@@ -31,20 +32,30 @@ if exist(edgef, 'file')
         title([pluginConfig.sensorName ' residual']);        
         
         xlim([0, edge(end,1)-edge(1,1)])
-        ylim([-3*yl 3*yl]);        
+        ylim([-3*yl-eps 3*yl+eps]);
         
         %axis tight
+        
+        % possibly plot events
+        if isfield(pluginConfig, 'events')
+            for j = 1:length(pluginConfig.events)
+                if (pluginConfig.events(j) >= edge(1,1) && pluginConfig.events(j) <= edge(end,1))                
+                    plot( (pluginConfig.events(j) - edge(1,1))* [1 1], ylim(), 'Color', [0.4, 0.4, 0.4] );                    
+                end
+            end
+        end     
 
         %% plot measure
         if (pluginConfig.measureSize > 0 && ~pluginConfig.errorOnly)         
             subplot('Position', squeezeArea([area(1) area(2)+area(4)*0.5, area(3) area(4)*0.5],0.02))
             
-            plot(edge(:,1) - edge(1,1),[edge(:,23:(23+pluginConfig.errorSize-1))])
+            plot(edge(:,1) - edge(1,1),[edge(:,23:(23+pluginConfig.errorSize-1))]);
             
             title([pluginConfig.sensorName ' measurement']);
         
-            axis tight
-        end
+            axis tight           
+        end        
+        
     end
     
 end
