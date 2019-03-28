dc2e497a35d15a7385f6f5cb4bb34440d032330f
Davide Antonio Cucci <davide.cucci@epfl.ch>
2019-03-28 10:21:09 +0100

    add a Plugin to plot the histogram of image observation residuals

---
+++ _development/Matlab/PluginViewer/plugins/EuclideanFeaturesStats.m
@@ -0,0 +1,37 @@
+function EuclideanFeatures( area, globalConfig, pluginConfig )
+
+subplot('Position', squeezeArea(area,0.02))
+hold on
+
+F = dir(globalConfig.logPath);
+
+measureSize = 2;
+errorSize = 2;
+
+err = [];
+
+for i = 1:length(F)            
+    % assume it is a Euclidean feature edge log, get feature id
+    F(i).name(length(pluginConfig.sensorName)+6:end);
+    n = sscanf(F(i).name(length(pluginConfig.sensorName)+6:end), '%d');
+
+    parf = sprintf('%s_feat%d.log', pluginConfig.sensorName, n);    
+
+    if (strcmp(F(i).name, parf)== true)
+        [edge, flag] = stubbornLoad([globalConfig.logPath parf]);
+
+        if flag == 1 && size(edge,1) > 0 % sometimes I got an empty p
+            err = [err; edge(:,(23+measureSize):(23+measureSize+errorSize-1))];
+        end
+    end            
+end
+
+title('img obs residuals (px)')
+
+errNorm = sqrt(sum(err.^2,2));
+hist(errNorm, max(errNorm)/0.1 );
+
+legend(sprintf('N = %d, mean = %.2f', length(errNorm), mean(errNorm)));
+
+end
+
