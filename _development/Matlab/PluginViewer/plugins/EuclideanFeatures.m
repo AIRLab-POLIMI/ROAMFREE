9aec88a4f0ab2570eea35a1c131cae97d08dcd41
Davide Antonio Cucci <davide.cucci@epfl.ch>
2019-06-26 11:11:29 +0200

    put nice time limits in EuclideanFeatures plugin

--- _development/Matlab/PluginViewer/plugins/EuclideanFeatures.m
+++ _development/Matlab/PluginViewer/plugins/EuclideanFeatures.m
@@ -12,13 +12,16 @@ errorSize = 2;
 
 legs = {};
 
+mt = +inf;
+Mt = -inf;
+
 for i = 1:length(F)            
     % assume it is a Euclidean feature edge log, get feature id
     F(i).name(length(pluginConfig.sensorName)+6:end);
     n = sscanf(F(i).name(length(pluginConfig.sensorName)+6:end), '%d');
 
     parf = sprintf('%s_feat%d.log', pluginConfig.sensorName, n);    
-
+    
     if (strcmp(F(i).name, parf)== true)
         legs{end+1} = sprintf('%d',n);
         
@@ -27,16 +30,21 @@ for i = 1:length(F)
         if flag == 1 && size(edge,1) > 0 % sometimes I got an empty p
             
             err = edge(:,(23+measureSize):(23+measureSize+errorSize-1));            
+
+            mt = min(mt, edge(1,1));
+            Mt = max(Mt, edge(end,1));
             
             subplot('Position', squeezeArea([area(1:3) area(4)*0.5],0.02))
             title('x')
             hold on
-            plot(edge(:,1), err(:,1), '.');
+            plot(edge(:,1), err(:,1), '-.');
+            xlim([mt Mt])
             
             subplot('Position', squeezeArea([area(1) area(2)+area(4)*0.5, area(3) area(4)*0.5],0.02))
             title('y')
             hold on
-            plot(edge(:,1), err(:,2), '.');
+            plot(edge(:,1), err(:,2), '-.');
+            xlim([mt Mt])
             
             %d = sqrt(sum(err.^2,2));                
             %plot(edge(:,1), d, '.');
