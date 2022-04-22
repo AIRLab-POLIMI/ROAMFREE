d1ae328b2e2e260040bd95b4f766a34ee1515740
Davide Antonio Cucci <davide.cucci@gmail.com>
2022-04-22 10:44:15 +0200

    removed debug coments

--- roamfree/ROAMestimation/src/Chi2TestAction.cpp
+++ roamfree/ROAMestimation/src/Chi2TestAction.cpp
@@ -21,11 +21,10 @@ void Chi2TestAction::reset() {
 g2o::HyperGraphAction* Chi2TestAction::operator()(const g2o::HyperGraph* graph, Parameters* parameters) {
     double chi2 = ((g2o::OptimizableGraph *)graph)->chi2();
 
-    std::cerr << "last: " << std::fixed << lastChi2 << " current: " << std::fixed << chi2 << " delta: " << std::fixed << (lastChi2 - chi2) << std::endl;
+    // std::cerr << "last: " << std::fixed << lastChi2 << " current: " << std::fixed << chi2 << " delta: " << std::fixed << (lastChi2 - chi2) << std::endl;
 
     if (lastChi2 > chi2 && (lastChi2 - chi2) < chi2threshold) {            
-
-        std::cerr << "Early stopping :)" << std::endl;
+        //std::cerr << "Early stopping :)" << std::endl;
         *stopFlag = true;
     }
 
