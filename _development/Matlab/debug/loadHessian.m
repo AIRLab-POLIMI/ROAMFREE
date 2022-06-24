f54e72b7db9d10b678344514c9174b4faae890b8
Davide Antonio Cucci <davide.cucci@gmail.com>
2022-06-24 16:00:45 +0200

    some hessian debugging code

--- _development/Matlab/debug/loadHessian.m
+++ _development/Matlab/debug/loadHessian.m
@@ -6,3 +6,28 @@ H_1_sp = spconvert(H);
 fprintf(' done.\n');
 
 %%
+dd = diag(H_1_sp);
+
+[m, im] = sort(dd, 'ascend');
+[M, iM] = sort(dd, 'descend');
+
+M(1:20)
+
+%%
+
+MAP = []
+
+% f = fopen('/tmp/roamfree/Hstruct.txt');
+f = fopen('/tmp/shiny/roamfree/Hstruct.txt');
+
+while ~feof(f)
+    l = fgetl(f);
+    
+    [tokens, match] = regexp(l, 'Camera_feat(\d+)_Lw\[\d+\.\d+\]: (\d+) to (\d+)', 'tokens', 'match');
+    if ~isempty(match) 
+       rows = str2double(tokens{1}{2}):str2double(tokens{1}{3});
+       fid = str2double(tokens{1}{1});
+       
+       MAP(rows+1) = fid;
+    end
+end
