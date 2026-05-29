50f11e1ac5e99e2bac97c15fc2c74981e3b7486c
Simon Gilgien <simon.gilgien@epfl.ch>
2026-05-29 15:15:08 +0200

    Allow to fix all poses in the graph.
    For use e.g. with a good INS/GNSS solution for boresight calibration.

--- roamfree/ROAMestimation/src/FactorGraphFilterImpl.cpp
+++ roamfree/ROAMestimation/src/FactorGraphFilterImpl.cpp
@@ -2009,6 +2009,12 @@ bool FactorGraphFilter_Impl::forgetOldNodes(double l) {
   return forgetNodes_i(toForget);
 }
 
+void FactorGraphFilter_Impl::setAllPosesFixed(bool fixed) {
+    for(PoseMapIterator it = _poses.begin(); it != _poses.end(); ++it) {
+	it->second->setFixed(fixed);
+    }
+}
+
 void FactorGraphFilter_Impl::deferMeasurement(struct Sensor& sensor, double t,
     const Eigen::VectorXd& z, const Eigen::MatrixXd& cov) {
   struct Measurement meas(sensor, t, z, cov);
